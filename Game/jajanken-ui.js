2e7b2e19593b5758f7f0c8bd866db041be455794
White-Panther420 <99096313+White-Panther420@users.noreply.github.com>
2022-06-23 12:40:24 -0700

    Fixed a bunch of stuff
    I wrote most of the CSS for the UI, the eventListener logic for the player's buttons, and figured out how to track the score and player selection for each round without them being reset (I used global variables).

--- Game/jajanken-ui.js
+++ Game/jajanken-ui.js
@@ -1,3 +1,27 @@
+//Selecing the player's buttons to add event listeners to them
+
+const rock_btn = document.querySelector(".rock");
+const paper_btn = document.querySelector(".paper");
+const scissors_btn = document.querySelector(".scissors");
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+rock_btn.addEventListener("click", function(e) {
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection)});
+    
+paper_btn.addEventListener("click", function(e) {
+    playerSelection = "paper";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection)});
+
+scissors_btn.addEventListener("click", function(e) {
+    playerSelection = "scissors";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection)});
+
 /*This function will generate a random number 1-3 and assin it either rock, paper, or scissors
 then return the result thus making it seem like you're playing against a computer*/
 function computerPlay()
@@ -6,105 +30,105 @@ function computerPlay()
 
     if(randomNumber === 1)
     {
-        const button = document.querySelector("C_rocl");
-        button.setAttribute("style", "background-color: red;")
-        return "Rock";
+        //const button1 = document.querySelector(".C_rock");
+        //button1.setAttribute("style", "background: red;");
+        return "rock";
     }
     else if(randomNumber === 2)
     {
-        return "Paper";
+        //const button2 = document.querySelector(".C_paper");
+        //button2.setAttribute("style", "background: pink;");
+        return "paper";
     }
     else    
-        return "Scissors";
+    {
+        //const button3 = document.querySelector(".C_scissors");
+        //button3.setAttribute("style", "background: green;");
+        return "scissors";
+    }
 }
 
 /*This function will calculate the winner of a round and return the result*/
-function winner(playerSelection = "rock", computerSelection)
+function playRound(playerSelection = "rock", computerSelection)
 {
     console.log(playerSelection);
     console.log(computerSelection);
     if(playerSelection === "paper" && computerSelection === "rock")
     {
-        return true;
+        roundResult = true;
     }
-
     else if(playerSelection === "rock" && computerSelection === "scissors")
     {
-        return true;
+        roundResult = true;
     }
-
     else if(playerSelection === "scissors" && computerSelection === "paper")
     {
-        return true;
+        roundResult = true;
     }
-
     else if(playerSelection === computerSelection)
     {
-        return "tie";
+        roundResult = "tie";
     }
     else if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors")  //An invalid option is entered
     {
-        return "error"
+        roundResult = "error"
     }
     else //Computer wins
     {
-        return false;
+        roundResult = false;
     }
+
+    game(roundResult);
 }
 
 /*This function will sumulate the game by taking in unput from the user and computer and 
 passing it to the winner function. Afterwards, it will take the result from the winner
 function and use it to calculate the score for each round*/
-function game()
+function game(roundResult)
 {
-    let roundResult;
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while(playerScore < 5 || computerScore < 5)
+    if(roundResult === true)
     {
-        let playerSelection = prompt("Please enter rock, paper, or scisors: ").toLowerCase();
-        let computerSelection = computerPlay().toLowerCase();
-        roundResult = winner(playerSelection, computerSelection);
-        
-        if(roundResult === true)
-        {
-            console.log("You win! Jajanken " + playerSelection + " beats jajanken " + computerSelection + "!");
-            playerScore++;
-        }
-        else if(roundResult === false)
-        {
-            console.log("You lose! Jajanken " + playerSelection + " beats jajanken " + computerSelection + "!");
-            computerScore++;
-        }
-        else if(roundResult === "error")
-        {
-            console.log("Invalid Option!");
-        }
-        else
-        {
-            console.log("tie!");
-        }
-
-        console.log("Player: " + playerScore);
-        console.log("Computer: " + computerScore)
+        console.log("You win! Jajanken " + playerSelection + " beats jajanken " + computerSelection + "!");
+        playerScore++;
     }
-
-    console.log("Game Over!!!");
-    console.log("Player: " + playerScore);
-    console.log("Computer: " + computerScore)
-    if(playerScore > computerScore)
+    else if(roundResult === false)
     {
-        console.log("YAY! YOU WUN!!!!!!!!!!");
+        console.log("You lose! Jajanken " + computerSelection + " beats jajanken " + playerSelection + "!");
+        computerScore++;
     }
-    else if(computerScore > playerScore)
+    else if(roundResult === "error")
     {
-        console.log("HAHA! GIT GUD SCRUB!");
+        console.log("Invalid Option!");
     }
     else
     {
-        console.log("A TIE!")
+        console.log("tie!");
+    }
+
+    console.log("Player: " + playerScore);
+    console.log("Computer: " + computerScore);
+    
+    if(playerScore >= 5)
+    {
+        console.log("You win!");
+        resetGame();
+        return;
+    }
+    else if(computerScore >= 5)
+    {
+        console.log("You lose!");
+        resetGame();
+        return;
+    }
+    else
+    {
+        console.log("ONTO THE NEXT ROUND!");
     }
 }
 
-game();
+function resetGame()
+{
+    playerScore = 0;
+    computerScore = 0;
+    console.clear;
+}
